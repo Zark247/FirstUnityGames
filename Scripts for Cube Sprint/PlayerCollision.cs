5701ccff36e1c9eee1e88dfd3dedd1753a8463f9
Jacob A. H <zark247@gmail.com>
2020-09-27 18:52:34 -0400

    Added project files for Cube Sprint
    I added the scripts used in Cube Sprint

---
+++ Scripts for Cube Sprint/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        //Debug.Log("We hit " + collisionInfo.collider.name);
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            //Debug.Log("We hit an " + collisionInfo.collider.name + "!");
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+            
+        }
+    }
+}
---
+++ Scripts for Cube Sprint/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rigidbody;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    bool left = false;
+    bool right = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Hello World!");
+        //rigidbody.useGravity = false;
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            left = true;
+        }
+        else if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            right = true;
+        }
+        
+    }
+
+    // FixedUpdate is called once per frame for PHYSICS
+    void FixedUpdate()
+    {
+        //Adds a forward force(x,y,z,forcemode) forward force(z)
+        rigidbody.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (left == true)
+        {
+            //Left direction
+            rigidbody.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            left = false;
+        }
+        else if (right == true)
+        {
+            //Right direction
+            rigidbody.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            right = false;
+        }
+        if(rigidbody.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
