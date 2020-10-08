e4ca5fb4319dfe7779a453198ceea93234489644
Jacob A. H <47677672+Zark247@users.noreply.github.com>
2020-10-08 13:05:31 -0400

    Added movement
    Added a script that moves the player in the world. Added a camera that will follow the player in the world.

---
+++ TestVita/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour {
+
+    public CharacterController controller;
+    public Transform cam;
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+	// Update is called once per frame
+	void Update () {
+        //simple movement
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        //direction vector comprised of an x,y,and z, normalized to not affect speed
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            //retruns the angle in which the player is facing
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            //this will smooth the turn angle, before it was snapping to face the direction
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            //faces the player in the direction they are traveling
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            //player will now face the direction of the camera when moving
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            //moves the player in the direction they have input, Time.deltaTime disregards the system specs so it runs the same on every system
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+	}
+}
